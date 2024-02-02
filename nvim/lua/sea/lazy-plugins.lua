fa837ada82dcad8e4f9c2f37098322fbb671c8da
Luis Neto <luis.neto.dev@gmail.com>
2024-02-02 16:09:29 +0000

    nvim update

--- nvim/lua/sea/lazy-plugins.lua
+++ nvim/lua/sea/lazy-plugins.lua
@@ -228,6 +228,31 @@ require('lazy').setup({
     dependencies = {
     }
   },
+  {
+    -- status line
+    'vim-airline/vim-airline',
+    dependencies = {
+    }
+  },
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- animation = true,
+      -- insert_at_start = true,
+      -- …etc.
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
+  {
+    'tpope/vim-eunuch',
+  },
+
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
