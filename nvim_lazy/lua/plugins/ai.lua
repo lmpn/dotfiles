240ab3f1f0b50c7fa12b49355e377ec33ae21ecb
lmpn <luis.manuel.neto@proton.me>
2025-03-07 08:55:42 +0000

    add ai

--- nvim_lazy/lua/plugins/ai.lua
+++ nvim_lazy/lua/plugins/ai.lua
@@ -9,6 +9,25 @@ return {
         -- provide an empty list to make provider available (no API key required)
         ollama = {},
       },
+      hooks = {
+        CodeConsultant = function(prt, params)
+          local chat_prompt = [[
+          Your task is to analyze the provided {{filetype}} code and suggest
+          improvements to optimize its performance. Identify areas where the
+          code can be made more efficient, faster, or less resource-intensive.
+          Provide specific suggestions for optimization, along with explanations
+          of how these changes can enhance the code's performance. The optimized
+          code should maintain the same functionality as the original code while
+          demonstrating improved efficiency.
+
+          Here is the code
+          ```{{filetype}}
+          {{filecontent}}
+          ```
+        ]]
+          prt.ChatNew(params, chat_prompt)
+        end,
+      },
     })
   end,
 }
